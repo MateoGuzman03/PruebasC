ff7f0920a0bb2fc746976eaa1c7f37a3ea15524b
MateoGuzman03 <82171386+MateoGuzman03@users.noreply.github.com>
2021-11-18 18:34:11 -0300

    Update ejercicio 16.c

--- ejercicio 16.c
+++ ejercicio 16.c
@@ -2,26 +2,23 @@
 #include <stdlib.h>
 
 int main() {
+int dos, tres;
 
-    int multi2, multi3;
-
-    multi2 = 0;
-    multi3 = 0;
+    dos = 0;
+    tres = 0;
 
         printf("Multiplos de 2\n");
         printf("\t\t\t\t\t\tMultiplos de 3\n");
-    while(multi2, multi3<100){
-        printf("\%i\n", multi2);
-        multi2+=2;
-        printf("\t\t\t\t\t\t%i\n", multi3);
-        multi3+=3;i3+=3;
+    while(dos<100 && tres<100){
+        printf("\%i\n", dos);
+        dos+=2;
+        printf("\t\t\t\t\t\t%i\n", tres);
+        tres+=3;
 
 
     }
 
-
-
-
+    
 
     return 0;
 }
