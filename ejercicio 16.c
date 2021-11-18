c8d5b5d9b529fb3b1517654d0cac632d37a6fad1
MateoGuzman03 <82171386+MateoGuzman03@users.noreply.github.com>
2021-11-18 18:19:18 -0300

    Add files via upload

---
+++ ejercicio 16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int multi2, multi3;
+
+    multi2 = 0;
+    multi3 = 0;
+
+    while(multi2, multi3<100){
+        printf("\tMultiplos de 2\n");
+        printf("\t%i\n", multi2);
+        multi2+=2;
+        printf("\tMultiplos de 3\n");
+        printf("%i\n", multi3);
+        multi3+=3;
+
+
+    }
+
+
+
+
+
+    return 0;
+}
