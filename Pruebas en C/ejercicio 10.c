1eb751d98cc27b438ca03286269bc539f33519f1
MateoGuzman03 <82171386+MateoGuzman03@users.noreply.github.com>
2021-11-18 13:17:35 -0300

    Update ejercicio 10.c

--- Pruebas en C/ejercicio 10.c
+++ Pruebas en C/ejercicio 10.c
@@ -3,44 +3,51 @@
 
 int main() {
 
-    int num,resp;
+    int num, resp, dia;
+    dia = 1;
 
-    printf("Ingrese un numero para elejir un dia de la semana, tener en cuenta que 1 es Lunes \n");
-    scanf("%i", &num);
 
-        switch(num) {
-            case 1: printf("Eligio el dia Lunes \n");
-                break;
-            case 2: printf("Eligio el dia Martes \n");
-                break;
-            case 3: printf("Eligio el dia Miercoles \n");
-                break;
-            case 4: printf("Eligio el dia Jueves \n");
-                break;
-            case 5: printf("Eligio el dia Viernes \n");
-                break;
-            case 6: printf("Eligio el dia Sabado \n");
-                break;
-            case 7: printf("Eligio el dia Domingo \n");
-                break;
 
-            default: printf("El numero que elijio no equivale a ningun dia de la semana");
-        }
-    for (num = 1; num < 8; num++)
+            printf("Ingrese un numero para elejir un dia de la semana, tener en cuenta que 1 es Lunes \n");
+            scanf("%i", &num);
+
+
+              switch(num) {
+                    case 1: printf("Eligio el dia Lunes \n");
+                        break;
+                    case 2: printf("Eligio el dia Martes \n");
+                        break;
+                    case 3: printf("Eligio el dia Miercoles \n");
+                        break;
+                    case 4: printf("Eligio el dia Jueves \n");
+                        break;
+                    case 5: printf("Eligio el dia Viernes \n");
+                        break;
+                    case 6: printf("Eligio el dia Sabado \n");
+                        break;
+                    case 7: printf("Eligio el dia Domingo \n");
+                        break;
+
+                    default: printf("El numero que elijio no equivale a ningun dia de la semana");
+                }
+
+
+
+    do
         {
-        printf("Hoy es el dia: %i, desea seguir trabajndo? Responda con 1 para seguir trabajando y con 0 para irse\n", num);
-        scanf("%i", &resp);
-        if(resp == 1)
-        {
-            printf("Sigue trabajando\n");
-        }
-        else if(resp == 0)
-        {
-            printf("Deja de trabajar \n");
-            break;
-        }
-    }
-
+            printf("Hoy es el dia: %i, desea seguir trabajndo? Responda con 1 para seguir trabajando y con 0 para irse\n", dia);
+            scanf("%i", &resp);
+            if(resp == 1)
+            {
+                dia++;
+                printf("Sigue trabajando\n");
+            }
+            else if(resp == 0)
+            {
+                printf("Deja de trabajar \n");
+                break;
+            }
+        } while (dia <=7);
 
 
     return 0;
