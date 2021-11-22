d4bc5b20eb0eded6546a0531606d970be38c7130
MateoGuzman03 <82171386+MateoGuzman03@users.noreply.github.com>
2021-11-22 17:52:52 -0300

    Update ejercicio 21.c

--- ejercicio 21.c
+++ ejercicio 21.c
@@ -7,160 +7,154 @@ int main() {
     suma = 0;
     prod = 1;
 
+   
     printf("\tIngrese un numero distinto a 0\n"); scanf("%i", &num1);
 
-    while(num1!=0){
-
-        if(num1<0){
-            prod = prod*num1;
-        }
-        else if (num1>0){
+    if (num1>0)
+      {
             suma = suma + num1;
+      }
+        else if(num1<0)
+        {
+            prod = prod * num1;
         }
-        break;
-    }
-
-    if(num1==0){
+    else
+    {
         printf("\tEl valor debe ser distinto a 0\n");
-        printf("\tIngrese un numero distinto a 0\n"); scanf("%i", &num1);
+        printf("\tIngrese un numero distinto00 a 0\n"); scanf("%i", &num1);
     }
-///////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////
     printf("\tIngrese un numero distinto a 0\n"); scanf("%i", &num2);
-
-    while (num2!=0){
-        if(num2<0){
-            prod = prod*num2;
-        }
-        else if (num2>0){
+    if (num2>0)
+      {
             suma = suma + num2;
+      }
+        else if(num2<0)
+        {
+            prod = prod * num2;
         }
-        break;
-    }
-    if(num2==0){
+    else
+    {
         printf("\tEl valor debe ser distinto a 0\n");
         printf("\tIngrese un numero distinto a 0\n"); scanf("%i", &num2);
     }
-///////////////////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////////
     printf("\tIngrese un numero distinto a 0\n"); scanf("%i", &num3);
-
-    while(num3!=0){
-         if(num3<0){
-            prod = prod*num3;
-        }
-        else if (num3>0){
+    if (num3>0)
+      {
             suma = suma + num3;
+      }
+        else if(num3<0)
+        {
+            prod = prod * num3;
         }
-        break;
-    }
-    if(num3==0){
+    else
+    {
         printf("\tEl valor debe ser distinto a 0\n");
         printf("\tIngrese un numero distinto a 0\n"); scanf("%i", &num3);
     }
-//////////////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////////////
     printf("\tIngrese un numero distinto a 0\n"); scanf("%i", &num4);
-    while(num4!=0){
-        if(num4<0){
-            prod = prod*num4;
-        }
-        else if (num4>0){
+    if (num4>0)
+      {
             suma = suma + num4;
+      }
+        else if(num4<0)
+        {
+            prod = prod * num4;
         }
-        break;
-    }
-    if(num4==0){
+    else
+    {
         printf("\tEl valor debe ser distinto a 0\n");
         printf("\tIngrese un numero distinto a 0\n"); scanf("%i", &num4);
     }
-//////////////////////////////////////////////////////////////////////////////////////////
-
+    ///////////////////////////////////////////////////////////////////////////////////////////
     printf("\tIngrese un numero distinto a 0\n"); scanf("%i", &num5);
-    while(num5!=0){
-        if(num5<0){
-            prod = prod*num5;
-        }
-        else if (num5>0){
+    if (num5>0)
+      {
             suma = suma + num5;
+      }
+        else if(num5<0)
+        {
+            prod = prod * num5;
         }
-        break;
-    }
-     if(num5==0){
+    else
+    {
         printf("\tEl valor debe ser distinto a 0\n");
         printf("\tIngrese un numero distinto a 0\n"); scanf("%i", &num5);
     }
-/////////////////////////////////////////////////////////////////////////////////////////////
-
+    /////////////////////////////////////////////////////////////////////////////////////////
     printf("\tIngrese un numero distinto a 0\n"); scanf("%i", &num6);
-    while(num6!=0){
-        if(num6<0){
-            prod = prod*num6;
-        }
-        else if (num6>0){
+    if (num6>0)
+      {
             suma = suma + num6;
+      }
+        else if(num6<0)
+        {
+            prod = prod * num6;
         }
-        break;
-    }
-     if(num6==0){
+    else
+    {
         printf("\tEl valor debe ser distinto a 0\n");
         printf("\tIngrese un numero distinto a 0\n"); scanf("%i", &num6);
     }
-/////////////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////////////
     printf("\tIngrese un numero distinto a 0\n"); scanf("%i", &num7);
-    while(num7!=0){
-        if(num7<0){
-            prod = prod*num7;
-        }
-        else if (num7>0){
+    if (num7>0)
+      {
             suma = suma + num7;
+      }
+        else if(num7<0)
+        {
+            prod = prod * num7;
         }
-        break;
-    }
-     if(num7==0){
+    else
+    {
         printf("\tEl valor debe ser distinto a 0\n");
         printf("\tIngrese un numero distinto a 0\n"); scanf("%i", &num7);
     }
-/////////////////////////////////////////////////////////////////////////////////////////
-
+    //////////////////////////////////////////////////////////////////////////////////////////////////
     printf("\tIngrese un numero distinto a 0\n"); scanf("%i", &num8);
-    while(num8!=0){
-        if(num8<0){
-            prod = prod*num8;
-        }
-        else if (num8>0){
+    if (num8>0)
+      {
             suma = suma + num8;
+      }
+        else if(num8<0)
+        {
+            prod = prod * num8;
         }
-        break;
-    }
-     if(num8==0){
+    else
+    {
         printf("\tEl valor debe ser distinto a 0\n");
         printf("\tIngrese un numero distinto a 0\n"); scanf("%i", &num8);
     }
-//////////////////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////////////////
     printf("\tIngrese un numero distinto a 0\n"); scanf("%i", &num9);
-    while(num9!=0){
-        if(num9<0){
-            prod = prod*num9;
-        }
-        else if (num9>0){
+    if (num9>0)
+      {
             suma = suma + num9;
+      }
+        else if(num9<0)
+        {
+            prod = prod * num9;
         }
-        break;
-    }
-     if(num9==0){
+    else
+    {
         printf("\tEl valor debe ser distinto a 0\n");
         printf("\tIngrese un numero distinto a 0\n"); scanf("%i", &num9);
     }
-/////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////
     printf("\tIngrese un numero distinto a 0\n"); scanf("%i", &num10);
-    while(num10!=0){
-        if(num10<0){
-            prod = prod*num10;
-        }
-        else if (num10>0){
+    if (num10>0)
+      {
             suma = suma + num10;
+      }
+        else if(num10<0)
+        {
+            prod = prod * num10;
         }
-        break;
-    }
-     if(num10==0){
+    else
+    {
         printf("\tEl valor debe ser distinto a 0\n");
         printf("\tIngrese un numero distinto a 0\n"); scanf("%i", &num10);
     }
