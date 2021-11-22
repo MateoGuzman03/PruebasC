b835f63f083db13770418f6447abcad93d2fb2c5
MateoGuzman03 <82171386+MateoGuzman03@users.noreply.github.com>
2021-11-22 17:40:35 -0300

    Add files via upload

---
+++ ejercicio 23.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int n, cuad, cub;
+
+    printf("\tIngrese un numero\n"); scanf("%i", &n);
+    cuad= n*n;
+    cub= n*n*n;
+
+    printf("Numero");
+
+    printf("\t\t\t\tCuadrado");
+
+    printf("\t\t\t\t\t\tCubo\n");
+
+    printf("%i", n);
+
+    printf("\t\t\t\t%i", cuad);
+
+    printf("\t\t\t\t\t\t\t%i", cub);
+
+
+
+
+    return 0;
+}
