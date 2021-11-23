8a2ec91340b64ae11014a400b66dcc645de291cf
MateoGuzman03 <82171386+MateoGuzman03@users.noreply.github.com>
2021-11-23 19:38:43 -0300

    Update ejercicio 25.c

--- ejercicio 25.c
+++ ejercicio 25.c
@@ -3,17 +3,11 @@
 
 int main() {
 
- int vend, bole=1, imp, impf1, impf2, impf3, comf1, comf2, comf3;
-  
+ int vend, bole=1, imp, impf1, impf2, impf3;
+
     impf1 = 0;
     impf2 = 0;
     impf3 = 0;
-
-    comf1 = 0;
-    comf2 = 0;
-    comf3 = 0;
-
-
 
   while(bole==1){
         printf("\tIngrese numero de vendedor\n"); scanf("\t%i", &vend);
@@ -21,19 +15,16 @@ int main() {
 
             printf("\tIngrese un importe de boleta\n"); scanf("\t%i", &imp);
             impf1 += imp;
-            comf1 +=imp*5/100;;
 
         }
         else if(vend==2){
             printf("\tIngrese un importe de boleta\n"); scanf("\t%i", &imp);
             impf2 += imp;
-            comf2 += imp*5/100;
 
         }
         else if(vend==3){
             printf("\tIngrese un importe de boleta\n"); scanf("\t%i", &imp);
             impf3 += imp;
-            comf3 += imp*5/100;
 
         }
         else{
@@ -42,18 +33,11 @@ int main() {
             printf("\tDesea ingresar otra boleta? Ingrese 1:SI 0:NO\n"); scanf("%i", &bole);
   }
 
-
-
-
     printf("\tVendedor"); printf("\t\t\tImporte"); printf("\t\t\t\tComision\n");
 
-    printf("\t1"); printf("\t\t\t\t%i", impf1); printf("\t\t\t\t%i\n", comf1);
-    printf("\t2"); printf("\t\t\t\t%i", impf2); printf("\t\t\t\t%i\n", comf2);
-    printf("\t3"); printf("\t\t\t\t%i", impf3); printf("\t\t\t\t%i\n", comf3);
-
-
-
-
+    printf("\t1"); printf("\t\t\t\t%i", impf1); printf("\t\t\t\t%i\n", impf1*5/100);
+    printf("\t2"); printf("\t\t\t\t%i", impf2); printf("\t\t\t\t%i\n", impf2*5/100);
+    printf("\t3"); printf("\t\t\t\t%i", impf3); printf("\t\t\t\t%i\n", impf3*5/100);
 
     return 0;
 }
