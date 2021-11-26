f40fcfbaa4c83dab50533555021bf0ea4a8d0370
MateoGuzman03 <82171386+MateoGuzman03@users.noreply.github.com>
2021-11-25 22:26:48 -0300

    Update ejercicio 26.c

--- ejercicio 26.c
+++ ejercicio 26.c
@@ -3,7 +3,7 @@
 
 int main() {
 
-    int lad1, lad2, lad3, sum1y2, sum2y3, sum1y3;
+     int lad1, lad2, lad3, sum1y2, sum2y3, sum1y3;
 
     printf("\tIngrese un largo para el lado 1 del triangulo\n"); scanf("%i", &lad1);
 
@@ -31,7 +31,6 @@ int main() {
     }
 
     else if(lad2==lad3){
-         //printf("\tEste es un triangulo Isosceles\n");
          if(sum2y3>lad1){
             printf("\tEste es un triangulo Isosceles\n");
          }
@@ -44,15 +43,6 @@ int main() {
             printf("\tEste es un triangulo Isosceles\n");
         }
     }
-    /*else if(sum1y2>lad3){
-            printf("\tEste es un triangulo Isosceles\n");
-        }
-    else if(sum2y3>lad1){
-            printf("\tEste es un triangulo Isosceles\n");
-         }
-    else if(sum1y3>lad2){
-        printf("\tEste es un triangulo Isosceles\n");
-    }*/
     else if(lad1!=lad2){
         if(lad1!=lad3){
             if(sum1y2>lad3){
@@ -69,11 +59,6 @@ int main() {
             }
         }
     }
-
-
-
-
-
 
     return 0;
 }
