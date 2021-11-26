697cc60ad690c7ede85079620d82921c86e39fae
MateoGuzman03 <82171386+MateoGuzman03@users.noreply.github.com>
2021-11-26 19:49:22 -0300

    Add files via upload

---
+++ ejercicio 28.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int num;
+
+    printf("\tIngrese un numero\n"); scanf("%i", &num);
+
+    printf("\t%ix1 = %i\n", num, num*1);
+    printf("\t%ix2 = %i\n", num, num*2);
+    printf("\t%ix3 = %i\n", num, num*3);
+    printf("\t%ix4 = %i\n", num, num*4);
+    printf("\t%ix5 = %i\n", num, num*5);
+    printf("\t%ix6 = %i\n", num, num*6);
+    printf("\t%ix7 = %i\n", num, num*7);
+    printf("\t%ix8 = %i\n", num, num*8);
+    printf("\t%ix9 = %i\n", num, num*9);
+    printf("\t%i10 = %i\n", num, num*10);
+
+
+
+
+    return 0;
+}
