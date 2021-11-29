79d6e799479e7c6595a227fe85c60ffcc2b51a91
MateoGuzman03 <82171386+MateoGuzman03@users.noreply.github.com>
2021-11-29 15:19:59 -0300

    Update funciones 03.c

--- funciones 03.c
+++ funciones 03.c
@@ -1,38 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int Mostrar();
-int Determinar();
+void Mostrar(char);
+char Determinar(int);
 
 void main() {
 
-    int num, mostrar ;
+    int num;
+    char resultado;
 
     printf("\tIngrese un numero\n"); scanf("%i", &num);
 
-    mostrar=Mostrar(num);
+    resultado = Determinar(num);
+    Mostrar(resultado);
 
-    printf("\t%i\n", mostrar);
-}
-int Mostrar(int num, int resultado){
-    int determinar = Determinar(num);
 
-    return resultado;
+}
+char Determinar(int num){
+    if(num<0)
+        return 'a';
+    if(num>0)
+        return 'b';
+
+    return 'c';
 }
 
-int Determinar(int num){
-    int resultado;
-    if(num==0){
-        resultado = printf("\tEs igual a 0\n");
+void Mostrar(char resultado){
+    switch(resultado){
+        case 'a': printf("\tNumero negativo");
+        break;
+        case 'b': printf("\tNumero positivo");
+        break;
+        case 'c': printf("\tIgual a 0");
+        break;
+        default: printf("\tLe mandaste cualquiera");
     }
-    else if(num>0){
-        resultado = printf("\tEs positivo\n");
-    }
-    else
-   {
-        if(num<0){
-                resultado = printf("\tEs negativo\n");
-        }
-    }
-    return resultado;
 }
+
