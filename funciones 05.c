47c4fe7b3f0d113536bcc1f1dfe15edf0ea0ed21
MateoGuzman03 <82171386+MateoGuzman03@users.noreply.github.com>
2021-11-30 10:13:51 -0300

    Update funciones 05.c

--- funciones 05.c
+++ funciones 05.c
@@ -1,26 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int IntercambioValores();
+int IntercambioValores1();
+int IntercambioValores2();
 
 void main() {
 
-    int num1, num2, inter;
+    int num1, num2;
 
     printf("\tIngrese un numero\n"); scanf("%i", &num1);
     printf("\tIngrese otro numero\n"); scanf("%i", &num2);
 
-    inter = IntercambioValores(num1, num2);
+    printf("\t%i",IntercambioValores1(num1, num2));
+    printf("\t%i",IntercambioValores2(num1, num2));
 
 }
 
-int IntercambioValores(int num1, int num2){
-    int num, inter;
-    num = num1;
+int IntercambioValores1(int num1, int num2){
     num1 = num2;
-    num2 = num;
-    inter = printf("\t %i %i", num1, num2);
-
-    return inter;
-
+    return num1;
 }
+
+int IntercambioValores2(int num1, int num2){
+    num2 = num1;
+    return num2;
+}
+
