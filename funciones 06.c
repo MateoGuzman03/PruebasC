576efb45907569069ba53009db20d130f953f8f2
MateoGuzman03 <82171386+MateoGuzman03@users.noreply.github.com>
2021-12-01 16:00:39 -0300

    Update funciones 06.c

--- funciones 06.c
+++ funciones 06.c
@@ -4,37 +4,39 @@
 char Menu();
 
 void main() {
-
-   Menu();
-
-
-}
-
-char Menu(){
-
-    char opcion;
-
-    printf("\tElija una opcion\n");
-    printf("\tOpcion A\n\tOpcion B\n\tOpcion C\n");
-
-    scanf("%c",&opcion);
-
-    switch(opcion){
-        case 'a' : printf("\tOpcion A");
-        break;
+    char menu;
+    menu = Menu();
+    switch(menu){
         case 'A' : printf("\tOpcion A");
         break;
-        case 'b' : printf("\tOpcion B");
-        break;
         case 'B' : printf("\tOpcion B");
-        break;
-        case 'c' : printf("\tOpcion C");
         break;
         case 'C' : printf("\tOpcion C");
         break;
         default : printf("\tEleji bien pedazo de tarado");
     }
+}
 
-    return opcion;
+char Menu(){
+    char eleccion, opcion;
+    printf("\tElija una opcion\n");
+    printf("\tOpcion A\n\tOpcion B\n\tOpcion C\n");
+    scanf("%c",&opcion);
+    switch(opcion){
+        case 'a' : eleccion = 'A';
+        break;
+        case 'A' : eleccion = 'A';
+        break;
+        case 'b' : eleccion = 'B';
+        break;
+        case 'B' : eleccion = 'B';
+        break;
+        case 'c' : eleccion = 'C';
+        break;
+        case 'C' : eleccion = 'C';
+        break;
+        default : printf("\tEleji bien pedazo de tarado");
+    }
+    return eleccion;
 
 }
